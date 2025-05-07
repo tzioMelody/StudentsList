dc6401b9ed830a25a49133a3a1a31602bcd62d5b
tzioMelody <140950404+tzioMelody@users.noreply.github.com>
2025-05-07 16:09:16 +0300

    Add project files.

---
+++ StudentsList/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentsList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Save_ClickBtn(object sender, RoutedEventArgs e)
+        {
+            string studentsFirstName = nameBox.Text;
+            string studentsLastName = lastnameBox.Text;
+
+            MessageBox.Show("Ο μαθητής " + studentsFirstName + " " + studentsLastName + " αποθηκεύτηκε με επιτυχία στην βάση!");
+
+        }
+
+        private void genderFilterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void GroupFilterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
